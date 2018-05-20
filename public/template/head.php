a19680ed06ef420cf18952bd2499645716a8ce42
Kamsiy <kamsiyoib@gmail.com>
2018-05-20 15:20:29 -0500

    SEO

--- public/template/head.php
+++ public/template/head.php
@@ -2,6 +2,15 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="isurc, ISUrc, ISU RC, Iowa State University, ISU Robotics Club, Robotics Club, Technology, Students, Computer Science," />
+<meta name="author" content="isurc" />
+<meta name="description" content="We are an on campus organization that provides a collaborative environment to build projects in automation and robotics" />
+<meta property="og:description" content="We are an on campus organization that provides a collaborative environment to build projects in automation and robotics" />
+<meta property="og:image" content="http://robotics.stuorg.iastate.edu/public/img/logo1.png" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:title" content="isurc | Iowa State University Robotics Club" />
+<meta property="og:url" content="http://robotics.stuorg.iastate.edu/" />
+<meta property="og:type" content="website" />
 <link rel="icon" href="public/img/logo1.png">
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous"> -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
