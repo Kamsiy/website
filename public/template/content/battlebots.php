45299d4d99914a76482494219d5729e8f3e7eb66
JACOB PRATT <isuandme@gmail.com>
2018-05-20 16:03:14 -0500

    set-up main layout for page ... need to get a buffer inbetween 'rows'

--- public/template/content/battlebots.php
+++ public/template/content/battlebots.php
@@ -1,5 +1,26 @@
-<section id="battlebots">
+<section id="snowplow">
     <div class="container">
-        <h2> BATTLE BOTS </h2>
+        <h2>
+            SNOWPLOW
+        </h2>
+
+        <div class="row">
+            <div class="col-lg-6">The Autonomous Snowplow is a project motivated by a competition in January where the goal is to clear a predefined section of snow using a plow no larger than two-meters square, running on its own power and controlled autonomously. This project requires mechanical, electrical, and software expertise, an original design, and all done by members of the Iowa State Robotic Club. More of this project can be seen at the competition website located at the URL in the bottom of this section. We’ve been working on this project for months, in order to ensure the best robot, we can build goes to competition. Below is a summary of our expected timeline.</div>
+            <div class="col-sm-1">  </div>
+            <div class="col-sm-5"> <img src="public/img/snowplow_v1.png"> </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-5"> <img src="public/img/snowplow_v1.png"> </div>
+            <div class="col-sm-1">  </div>
+            <div class="col-lg-6">The Autonomous Snowplow is a project motivated by a competition in January where the goal is to clear a predefined section of snow using a plow no larger than two-meters square, running on its own power and controlled autonomously. This project requires mechanical, electrical, and software expertise, an original design, and all done by members of the Iowa State Robotic Club. More of this project can be seen at the competition website located at the URL in the bottom of this section. We’ve been working on this project for months, in order to ensure the best robot, we can build goes to competition. Below is a summary of our expected timeline.</div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">The Autonomous Snowplow is a project motivated by a competition in January where the goal is to clear a predefined section of snow using a plow no larger than two-meters square, running on its own power and controlled autonomously. This project requires mechanical, electrical, and software expertise, an original design, and all done by members of the Iowa State Robotic Club. More of this project can be seen at the competition website located at the URL in the bottom of this section. We’ve been working on this project for months, in order to ensure the best robot, we can build goes to competition. Below is a summary of our expected timeline.</div>
+            <div class="col-sm-1">  </div>
+            <div class="col-sm-5"> <img src="public/img/snowplow_v1.png"> </div>
+        </div>
+
     </div>
 </section>
