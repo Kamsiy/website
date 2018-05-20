cf2a2b0fe03b86b8d50c21e8da8c43fb025535b3
JACOB PRATT <isuandme@gmail.com>
2018-05-20 18:37:42 -0500

    updates with two new photots and the snowplow.php file in the content folder.

--- public/template/content/snowplow.php
+++ public/template/content/snowplow.php
@@ -4,17 +4,17 @@
             SNOWPLOW
         </h2>
         <div class="row" style="margin-bottom: 50px;" align="right">
-            <div class="col-lg-5">The purpose of this project to design, build, and operate a fully autonomous snowplow for the purpose of removing snow from a designated path. This project requires mechanical, electrical, and software expertise. A</div>
-            <div class="col-sm-6"> <img src="public/img/snowplow_v1.png" style="margin-left: 50px;"> </div>
+            <div class="col-lg-5"> The purpose of this project is to design, build and operate a fully autonomous snowplow for the purpose of removing snow from a designated path. This project requires mechanical, electrical, and software expertise. What makes this project unique in the world of robotics is its heavy focus on path planning combined with location sensing and obstacle avoidance. All of this is combined in an environment where mechanical ability and sensors can be highly limited. With the rise of autonomous vehicles, the importance of solving these problems is at an all-time high. In this project, we aim to provide a unique solution to these problems in an attempt to advance such an important field of research.  </div>
+            <div class="col-sm-6"> <img src="public/img/snowplow_v2.png" style="margin-left: 50px;"> </div>
         </div>
 
         <div class="row" style="margin-bottom: 50px;">
-            <div class="col-sm-5"> <img src="public/img/snowplow_v1.png" > </div>
-            <div class="col-lg-6" style="margin-left: 50px;">The Autonomous Snowplow is a project motivated by a competition in January where the goal is to clear a predefined section of snow using a plow no larger than two-meters square, running on its own power and controlled autonomously. This project requires mechanical, electrical, and software expertise, an original design, and all done by members of the Iowa State Robotic Club. More of this project can be seen at the competition website located at the URL in the bottom of this section. We’ve been working on this project for months, in order to ensure the best robot, we can build goes to competition. Below is a summary of our expected timeline. </div>
+            <div class="col-sm-5"> <img src="public/img/snowplow_v3.png" > </div>
+            <div class="col-lg-6" style="margin-left: 60px;" align="left"> Last year two major events happened that set this project up for success. First, we designed and built, thanks to our sponsors, a plow capable of competing in the most rigorous winter environments. This plow was designed with the idea that it would be used in many future competitions. Second, we brought nine team members to the ION Snowplow competition in St. Paul Minnesota, where we competed against seven other teams. Although we weren't able to take home the win, we learned a great deal about what it took to compete and brought home an endless supply of ideas for the following year. </div>
         </div>
 
         <div class="row" style="margin-bottom: 50px;" >
-            <div class="col-lg-6">The Autonomous Snowplow is a project motivated by a competition in January where the goal is to clear a predefined section of snow using a plow no larger than two-meters square, running on its own power and controlled autonomously. This project requires mechanical, electrical, and software expertise, an original design, and all done by members of the Iowa State Robotic Club. More of this project can be seen at the competition website located at the URL in the bottom of this section. We’ve been working on this project for months, in order to ensure the best robot, we can build goes to competition. Below is a summary of our expected timeline.</div>
+            <div class="col-lg-6">  </div>
             <div class="col-sm-6"> <img src="public/img/snowplow_v1.png"> </div>
         </div>
 
