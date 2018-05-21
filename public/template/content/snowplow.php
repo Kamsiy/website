c909526afa3c04b687a2511538f63c28b7cdbf4f
JACOB PRATT <isuandme@gmail.com>
2018-05-20 19:01:14 -0500

    Update pictures in the projects page on the snowplow section.

--- public/template/content/snowplow.php
+++ public/template/content/snowplow.php
@@ -3,19 +3,19 @@
         <h2>
             SNOWPLOW
         </h2>
-        <div class="row" style="margin-bottom: 50px;" align="right">
+        <div class="row" style="margin-bottom: 100px;" align="right">
             <div class="col-lg-5"> The purpose of this project is to design, build and operate a fully autonomous snowplow for the purpose of removing snow from a designated path. This project requires mechanical, electrical, and software expertise. What makes this project unique in the world of robotics is its heavy focus on path planning combined with location sensing and obstacle avoidance. All of this is combined in an environment where mechanical ability and sensors can be highly limited. With the rise of autonomous vehicles, the importance of solving these problems is at an all-time high. In this project, we aim to provide a unique solution to these problems in an attempt to advance such an important field of research.  </div>
             <div class="col-sm-6"> <img src="public/img/snowplow_v2.png" style="margin-left: 50px;"> </div>
         </div>
 
-        <div class="row" style="margin-bottom: 50px;">
-            <div class="col-sm-5"> <img src="public/img/snowplow_v3.png" > </div>
-            <div class="col-lg-6" style="margin-left: 60px;" align="left"> Last year two major events happened that set this project up for success. First, we designed and built, thanks to our sponsors, a plow capable of competing in the most rigorous winter environments. This plow was designed with the idea that it would be used in many future competitions. Second, we brought nine team members to the ION Snowplow competition in St. Paul Minnesota, where we competed against seven other teams. Although we weren't able to take home the win, we learned a great deal about what it took to compete and brought home an endless supply of ideas for the following year. </div>
+        <div class="row" style="margin-bottom: 100px;">
+            <div class="col-sm-7"> <img src="public/img/snowplow_team.png" style="margin-top: 10px;"> </div>
+            <div class="col-lg-4" style="margin-left: 60px;" align="left"> Last year two major events happened that set this project up for success. First, we designed and built, thanks to our sponsors, a plow capable of competing in the most rigorous winter environments. This plow was designed with the idea that it would be used in many future competitions. Second, we brought ten team members to the ION Snowplow Competition in St. Paul Minnesota, where we competed against seven other teams. Although we weren't able to take home the win, we took home an endless supply of ideas for the following year. </div>
         </div>
 
         <div class="row" style="margin-bottom: 50px;" >
             <div class="col-lg-6">  </div>
-            <div class="col-sm-6"> <img src="public/img/snowplow_v1.png"> </div>
+            <div class="col-sm-6"> <img src="public/img/snowplow_v3.png"> </div>
         </div>
 
     </div>
